ab50526599914b35164e1294c19a95d772ede32e
fodela <particleme30@gmail.com>
2022-10-28 18:31:00 +0000

    refactor: remove task state from models for  simplicity

--- taskmanager/todo_app/models.py
+++ taskmanager/todo_app/models.py
@@ -1,13 +1,10 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 
 
-class TaskState(models.Model):
-    id = models.AutoField(primary_key=True)
-    is_completed = models.BooleanField(default=False)
-    is_due = models.BooleanField(default=False)
-
-    def __repr__(self) -> str:
-        return f"<<TaskState | ID: {self.id} >>"
+def tomorrow():
+    return timezone.now() + timedelta(days=1)
 
 
 class TaskCategory(models.Model):
@@ -21,10 +18,11 @@ class TaskCategory(models.Model):
 class Task(models.Model):
     id = models.AutoField(primary_key=True)
     description = models.CharField(max_length=264)
-    state = models.ForeignKey(TaskState, on_delete=models.CASCADE)
     category = models.ForeignKey(TaskCategory, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    due_on = models.DateTimeField(default=tomorrow)
+    is_completed = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return f"<<Task : {self.description}>>"
