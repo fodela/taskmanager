580ffc0584f1f6c23c5072ad5b0a59d5aace2447
fodela <particleme30@gmail.com>
2022-10-28 11:26:29 +0000

    register models to admin

--- taskmanager/todo_app/admin.py
+++ taskmanager/todo_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from models import Task, TaskCategory, TaskState
 # Register your models here.
+
+admin.register(Task)
+admin.register(TaskCategory)
+admin.register(TaskState)
