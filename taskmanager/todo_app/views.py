e4fceb232c72299e5feb199b74137f809393f0d5
fodela <particleme30@gmail.com>
2022-10-30 22:36:00 +0000

    feat: add task in index

--- taskmanager/todo_app/views.py
+++ taskmanager/todo_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from todo_app.models import Task
 
 # Create your views here.
+
+
+def index(request):
+    task_list = Task.objects.order_by("created_at")
+    task_dict = {"all_task": task_list}
+    return render(request, "todo_app/index.html", context=task_dict)
+
+
+def project(request):
+    return render(request, "todo_app/project.html")
+
+
+def task(request):
+    return render(request, "todo_app/task.html")
